bef2aba9a14b061df86cf49102eec4b3d1790ea5
AdityaNijave <adityanijave09@gmail.com>
2024-08-07 16:11:27 +0530

    learning how to use different method of rounding a number like 'ceil', 'floor', and 'round'

---
+++ numberRoundingMethods.m
@@ -0,0 +1,18 @@
+%learning how to different method of rounding a number
+
+%consider we are having number mention below for rounding it
+number  = 13.5;
+
+%ceil: for upstair rounding
+display("rounding using 'ceil' of number " + number + " is " + ceil(13.5));
+
+
+%floor: for downstair rounding
+display("rounding using 'floor' of number " + number + " is " + floor(13.5));
+
+%round: just round the number as per rules
+display("rounding using 'round' of number " + number + " is " + round(13.5));
+
+
+%note: you can see the result in workspace
+disp("program run sucessfully.");
