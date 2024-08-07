91796c0e65c4ea78115b6b484a910591b3f7f50d
AdityaNijave <adityanijave09@gmail.com>
2024-08-07 22:35:08 +0530

    learning how to access element of row vector and column vector

---
+++ access_elementOfVector.m
@@ -0,0 +1,24 @@
+%learning how to access element of vector
+
+% accessing elements of row vector
+r_vec = [11 12 155 8415 6512];
+disp("The row vector is:");
+disp(r_vec);
+
+disp("The 1st element of row vector is " + r_vec(1));
+disp("The 2nd element of row vector is " + r_vec(2));
+disp("The 3rd element of row vector is " + r_vec(3));
+disp("The 4th element of row vector is " + r_vec(4));
+disp("The 5th element of row vector is " + r_vec(5));
+
+
+% accessing elements of col vector
+c_vec = [89651; 5 ;51; 85; 45];
+disp("The col vector is:");
+disp(c_vec);
+
+disp("The 1st element of col vector is " + c_vec(1));
+disp("The 2nd element of col vector is " + c_vec(2));
+disp("The 3rd element of col vector is " + c_vec(3));
+disp("The 4th element of col vector is " + c_vec(4));
+disp("The 5th element of col vector is " + c_vec(5));
