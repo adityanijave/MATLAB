154adf8136d684e4e2af12007b387995914583dd
AdityaNijave <adityanijave09@gmail.com>
2024-08-07 15:27:48 +0530

    Learned use of ':' for range

---
+++ useOf_rangeOfVariables.m
@@ -0,0 +1,10 @@
+%display the range of variables
+
+%asending order
+asending_order = 1:1:10;
+
+%desending_order
+desending_order = 10:-2:1;
+
+%note: you can see the result in workspace
+disp("program run sucessfully.");
