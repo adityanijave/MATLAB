5b96374ecc0d7fc9f2a0524a7cf01f0bf640b7a8
AdityaNijave <adityanijave09@gmail.com>
2024-08-07 22:45:57 +0530

    learning how to access the elements of matrix

---
+++ access_elementOfMatrix.m
@@ -0,0 +1,17 @@
+%learning how to access element of matrix of size mxn
+
+% declaring the matrix of size 2x3
+mat = [11 12 155; 1:1:3];
+disp("The matrix is:");
+disp(mat);
+
+%accessing the elements of row no 1
+disp("The 1st element of row one is " + mat(1,1));
+disp("The 2nd element of row one is " + mat(1,2));
+disp("The 3rd element of row one is " + mat(1,3));
+
+
+%accessing the elements of row no 2
+disp("The 1st element of row two is " + mat(2,1));
+disp("The 2nd element of row two is " + mat(2,2));
+disp("The 3rd element of row two is " + mat(2,3));
