500f7fc16492a1a6c6095ab7a2afa524ae8e28c4
AdityaNijave <adityanijave09@gmail.com>
2024-08-07 15:50:37 +0530

    learned how to use 'linspace' and its purpose

--- useOf_linspace.m
+++ useOf_linspace.m
@@ -3,7 +3,10 @@
 %linspace(x1, x2,n)
 % where,   x1 = start point (point interval), x2 = , end point (point interval) and n = number of points we want
 %If you provide n, it generates n points. The spacing between the points is (x2 - x1) / (n - 1).
+% and if value of n points, then it will do 100 even part for the same intervalue points
 
+linspace_variable_with_n = linspace(2, 20, 5);
+linspace_variable_without_n = linspace(1, 100);
 
-linspace_variable = linspace(2, 20, 5);
-disp(linspace_variable);
+%note: you can see the result in workspace
+disp("program run sucessfully.");
