fa6f06e5fe4a430cea68957b15c0e696780604aa
AdityaNijave <adityanijave09@gmail.com>
2024-08-07 22:03:16 +0530

    learn how to create the row and column vector in matlab

---
+++ creating_rowAndColumnVector.m
@@ -0,0 +1,13 @@
+% creating row and column vectors
+
+%row vector
+r_vec = [1 2 3 4 5];
+
+disp("The row vector is ");
+disp(r_vec);
+
+
+% column vector
+c_vector = [6;7;8;9;10];
+disp("The column vector is ");
+disp(c_vector);
