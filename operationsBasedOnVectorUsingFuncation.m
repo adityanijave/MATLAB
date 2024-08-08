c275f34dd1b358145d9c76848b2d47f0934b1647
AdityaNijave <adityanijave09@gmail.com>
2024-08-08 15:33:51 +0530

    Operations Based On Vector Using Different Default Funcations like min, max, product, and sum

---
+++ operationsBasedOnVectorUsingFuncation.m
@@ -0,0 +1,17 @@
+% Operations Based On Vector Using Different Default Funcations
+
+vec = 1:10;
+disp("Vector having elements are: ");
+disp(vec);
+
+%finding max no from the vector
+disp("the max number is " + max(vec));
+
+%finding min no from the vector
+disp("the min number is "+ min(vec));
+
+%finding the sum of vectors elements
+disp("the sum of vector "+ sum(vec));
+
+%finding the product of vectors elements
+disp("the product of vectors element is: "+ prod(vec)); 
