dbab430b0fbe4a5494b56bab429f1c094f89ee81
AdityaNijave <adityanijave09@gmail.com>
2024-08-08 20:25:25 +0530

    Display the value of variables using different matlab funcation like disp() and fprint()

---
+++ displayValueOfVariables.m
@@ -0,0 +1,8 @@
+% learning the different method to display the values from script
+
+% method 1: using disp() funcation of matlab
+disp("This message is printed using the disp() funcation");
+
+% method 2: using fprintf() funcation of matlab
+x = 12;
+fprintf("\n\nThe value of x is %d\nAbove and this message is printed using the fprintf() funcation\n", x);
